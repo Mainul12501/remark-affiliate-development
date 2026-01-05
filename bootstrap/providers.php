4ba78762101a38e2555d60d9daf666c65b625dc9
Rezaul890 <rezaulmba333@gmail.com>
2026-01-05 13:37:05 +0600

    install yajra datatable

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     \Uzzal\Acl\AclServiceProvider::class,
+    \Yajra\DataTables\DataTablesServiceProvider::class,
 ];
